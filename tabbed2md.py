3ddbfbdf13f8293d6b912ba086d1802af16a5dae
Mark Sprevak <sprevak.home@gmail.com>
2015-11-09 09:08:56 +0000

    minor refactor

--- tabbed2md.py
+++ tabbed2md.py
@@ -18,24 +18,22 @@ def main():
 
 def process(in_lines):
     out_lines = remove_cr(in_lines)
-    out_lines = split_by_col(out_lines)
-    out_lines = remove_whitespace(out_lines)
+    out_lines = read(out_lines)
+    out_lines = strip_whitespace(out_lines)
     out_lines = add_missing_cols(out_lines)
-    max_lens = find_max_entries(out_lines)
-    out_lines = add_spaces(out_lines, max_lens)
-    out_lines = delimit_pandoc(out_lines, max_lens)
-    out_lines = rejoin(out_lines)
+    max_lens = find_max_lens(out_lines)
+    out_lines = write_pandoc(out_lines, max_lens)
     return out_lines
 
 def remove_cr(in_lines):
     out_lines = [j.strip() for j in in_lines]
     return out_lines
 
-def split_by_col(in_lines):
+def read(in_lines):
     out_lines = [j.split('\t') for j in in_lines]
     return out_lines
 
-def remove_whitespace(in_lines):
+def strip_whitespace(in_lines):
     out_lines = [[i.strip() for i in j] for j in in_lines]
     return out_lines
 
@@ -49,7 +47,7 @@ def add_missing_cols(in_lines):
         out_lines.append(out)
     return out_lines
 
-def find_max_entries(in_lines):
+def find_max_lens(in_lines):
     max_lens = [1] * len(in_lines[0])
     for j in in_lines:
         for n, i in enumerate(j):
@@ -58,22 +56,31 @@ def find_max_entries(in_lines):
     max_lens = [i + OVERHANG for i in max_lens]
     return max_lens
 
-def add_spaces(in_lines, max_lens):
-    out_lines = list()
-    for j in in_lines:
-        out = [str(i).ljust(max_lens[n]) for n, i in enumerate(j)]
-        out_lines.append(out)
-    return out_lines
+def write_pandoc(in_lines, max_lens):
+    # write pandoc-style tables
+    # http://pandoc.org/README.html#tables
 
-def delimit_pandoc(in_lines, max_lens):
-    # add delimiters for pandoc tables
-    d = ['-' * n for n in max_lens]
-    out_lines = [d] + in_lines + [d]
-    return out_lines
+    def pad_cells(in_lines, max_lens):
+        out_lines = list()
+        for j in in_lines:
+            out = [str(i).ljust(max_lens[n]) for n, i in enumerate(j)]
+            out_lines.append(out)
+        return out_lines
 
-def rejoin(in_lines):
-    sep = ' ' * PADDING
-    out_lines = [sep.join(j) + '\n' for j in in_lines]
+    def delimit_start_end(in_lines, max_lens):
+        # add delimiters for pandoc tables
+        d = ['-' * n for n in max_lens]
+        out_lines = [d] + in_lines + [d]
+        return out_lines
+
+    def rejoin(in_lines):
+        sep = ' ' * PADDING
+        out_lines = [sep.join(j) + '\n' for j in in_lines]
+        return out_lines
+
+    out_lines = pad_cells(in_lines, max_lens)
+    out_lines = delimit_start_end(out_lines, max_lens)
+    out_lines = rejoin(out_lines)
     return out_lines
 
 if __name__ == '__main__':
